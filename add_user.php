0b58e1a13c2524d22ae395bfea224cdd783e96f6
wei <lin951016@gmail.com>
2023-11-13 12:15:05 +0800

    新增adduser

---
+++ add_user.php
@@ -0,0 +1,9 @@
+<?php
+$dsn = "mysql:host=localhost;charset=utf8;dbname=member";
+$pdo = new PDO($dsn,'root','');
+
+$sql = "insert into `users`(`acc`,`pw`,`name`,`email`,`address`)
+                     values('{$_POST['acc']}','{$_POST['pw']}','{$_POST['name']}',
+                     '{$_POST['email']}','{$_POST['address']}')";
+
+$pdo->exec($sql);
